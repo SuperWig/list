0b53206b3b27cda7b115ed22b9a49fa71c11a89f
Daniel Marshall <xandan@gmail.com>
2018-12-05 18:35:14 +0000

    Added .clang-format and static_asserts for trivially_copyable

--- LinkedList/list.h
+++ LinkedList/list.h
@@ -1,304 +1,370 @@
 #pragma once
 #include <initializer_list>
 
-template<class T>
+template <class T>
 class list
 {
-	//forward declarations;
-	struct iterator;
-	struct const_iterator;
-	template<class>
-	friend void swap(list& lhs, list& rhs) noexcept;
-	// types
-	using value_type = T;
-	using pointer = T*;
-	using const_pointer = const T*;
-	using reference = value_type& ;
-	using const_reference = const value_type&;
-	using size_type = std::size_t;
-	using difference_type = std::ptrdiff_t;
-	using reverse_iterator = std::reverse_iterator<iterator>;
-	using const_reverse_iterator = std::reverse_iterator<const_iterator>;
+    template <class>
+    friend void swap(list& lhs, list& rhs) noexcept;
 
-	struct node;
-	struct node_base
-	{
-		node_base() : prev(this), next(this) {}
-		node_base(node_base* p, node_base* n) : prev(p), next(n) {}
-		[[nodiscard]] node* as_node() { return static_cast<node*>(this); }
-		[[nodiscard]] const node* as_node() const { return static_cast<const node*>(this); }
-		node_base* prev;
-		node_base* next;
-	};
-	struct node : node_base
-	{
-		node(node_base* prev, node_base* next, const T& val) : node_base{ prev, next }, data(val) {}
-		template<typename...Args>
-		node(node_base* prev, node_base* next, Args&&... val) : node_base{ prev, next }, data(std::forward<Args>(val)...) {}
-		T data;
-	};
-	template<class ...Args>
-	node* add_node(node_base* prev, node_base* next, Args&&... val)
-	{
-		auto* new_node = new node(prev, next, std::forward<Args>(val)...);
-		new_node->next->prev = new_node;
-		new_node->prev->next = new_node;
-		++size_;
-		return new_node;
-	}
-	void delete_node(node_base* node);
+    struct node;
+    struct node_base
+    {
+        node_base()
+            : prev(this)
+            , next(this)
+        {
+        }
+        node_base(node_base* p, node_base* n)
+            : prev(p)
+            , next(n){}[[nodiscard]] node * as_node()
+        {
+            return static_cast<node*>(this);
+        }
+        [[nodiscard]] const node* as_node() const { return static_cast<const node*>(this); } node_base* prev;
+        node_base* next;
+    };
+    struct node : node_base
+    {
+        node(node_base* prev, node_base* next, const T& val)
+            : node_base{prev, next}
+            , data(val)
+        {
+        }
+        template <typename... Args>
+        node(node_base* prev, node_base* next, Args&&... val)
+            : node_base{prev, next}
+            , data(std::forward<Args>(val)...)
+        {
+        }
+        T data;
+    };
+    template <class... Args>
+    node* add_node(node_base* prev, node_base* next, Args&&... val)
+    {
+        auto* new_node       = new node(prev, next, std::forward<Args>(val)...);
+        new_node->next->prev = new_node;
+        new_node->prev->next = new_node;
+        ++size_;
+        return new_node;
+    }
+    void delete_node(node_base* node);
 
-	node_base end_;
-	std::size_t	size_;
+    node_base end_;
+    std::size_t size_;
+
 public:
-	list() : end_{}, size_(0) {}
-	list(std::initializer_list<T> init);
-	~list();
-	[[nodiscard]] iterator begin() noexcept { return end_.next; }
-	[[nodiscard]] const_iterator begin() const noexcept { return end_.next; }
-	[[nodiscard]] const_iterator cbegin() const noexcept { return end_.next; }
-	[[nodiscard]] iterator end() noexcept { return &end_; }
-	[[nodiscard]] const_iterator end() const noexcept { return &end_; }
-	[[nodiscard]] const_iterator cend() const noexcept { return &end_; }
-	[[nodiscard]] reverse_iterator rbegin() noexcept { return reverse_iterator(end()); }
-	[[nodiscard]] const_reverse_iterator rbegin() const noexcept { return const_reverse_iterator(end()); }
-	[[nodiscard]] const_reverse_iterator crbegin() const noexcept { return const_reverse_iterator(end()); }
-	[[nodiscard]] reverse_iterator rend() noexcept { return reverse_iterator(begin()); }
-	[[nodiscard]] const_reverse_iterator rend() const noexcept { return const_reverse_iterator(begin()); }
-	[[nodiscard]] const_reverse_iterator crend() const noexcept { return const_reverse_iterator(begin()); }
+    // forward declarations;
+    struct iterator;
+    struct const_iterator;
+    // types
+    using value_type             = T;
+    using pointer                = T*;
+    using const_pointer          = const T*;
+    using reference              = value_type&;
+    using const_reference        = const value_type&;
+    using size_type              = std::size_t;
+    using difference_type        = std::ptrdiff_t;
+    using reverse_iterator       = std::reverse_iterator<iterator>;
+    using const_reverse_iterator = std::reverse_iterator<const_iterator>;
 
-	[[nodiscard]] reference front();
-	[[nodiscard]] const_reference front() const;
-	[[nodiscard]] reference back();
-	[[nodiscard]] const_reference back() const;
+    list()
+        : end_{}
+        , size_(0)
+    {
+    }
+    list(std::initializer_list<T> init);
+    ~list();
+    [[nodiscard]] iterator begin() noexcept { return end_.next; }
+    [[nodiscard]] const_iterator begin() const noexcept { return end_.next; }
+    [[nodiscard]] const_iterator cbegin() const noexcept { return end_.next; }
+    [[nodiscard]] iterator end() noexcept { return &end_; }
+    [[nodiscard]] const_iterator end() const noexcept { return &end_; }
+    [[nodiscard]] const_iterator cend() const noexcept { return &end_; }
+    [[nodiscard]] reverse_iterator rbegin() noexcept { return reverse_iterator(end()); }
+    [[nodiscard]] const_reverse_iterator rbegin() const noexcept { return const_reverse_iterator(end()); }
+    [[nodiscard]] const_reverse_iterator crbegin() const noexcept { return const_reverse_iterator(end()); }
+    [[nodiscard]] reverse_iterator rend() noexcept { return reverse_iterator(begin()); }
+    [[nodiscard]] const_reverse_iterator rend() const noexcept { return const_reverse_iterator(begin()); }
+    [[nodiscard]] const_reverse_iterator crend() const noexcept { return const_reverse_iterator(begin()); }
 
-	[[nodiscard]] std::size_t size() const noexcept { return size_; }
-	[[nodiscard]] bool empty() const noexcept { return !size_; }
+    [[nodiscard]] reference front();
+    [[nodiscard]] const_reference front() const;
+    [[nodiscard]] reference back();
+    [[nodiscard]] const_reference back() const;
 
-	void push_back(const T& val);
-	void push_back(T&& val);
-	void push_front(const T& val);
-	void push_front(T&& val);
-	iterator insert(const_iterator pos, const T& val);
-	iterator insert(const_iterator pos, T&& val);
+    [[nodiscard]] std::size_t size() const noexcept { return size_; }
+    [[nodiscard]] bool empty() const noexcept { return !size_; }
 
-	template<class... Args>
-	reference emplace_back(Args&&... args);
-	template<class... Args>
-	reference emplace_front(Args&&... args);
-	template<class... Args>
-	iterator emplace(const_iterator pos, Args&&... args);
+    void push_back(const T& val);
+    void push_back(T&& val);
+    void push_front(const T& val);
+    void push_front(T&& val);
+    iterator insert(const_iterator pos, const T& val);
+    iterator insert(const_iterator pos, T&& val);
 
-	void reverse() noexcept;
-	void swap(list& other) noexcept;
+    template <class... Args>
+    reference emplace_back(Args&&... args);
+    template <class... Args>
+    reference emplace_front(Args&&... args);
+    template <class... Args>
+    iterator emplace(const_iterator pos, Args&&... args);
+
+    void reverse() noexcept;
+    void swap(list& other) noexcept;
 };
 
-template<class T>
-list<T>::list(std::initializer_list<T> init) : size_(init.size())
+template <class T>
+list<T>::list(std::initializer_list<T> init)
+    : size_(init.size())
 {
-	for(auto it = init.begin(); it != init.end(); ++it)
-	{
-		push_back(*it);
-	}
+    for (auto it = init.begin(); it != init.end(); ++it)
+    {
+        push_back(*it);
+    }
 }
 
 template <class T>
 list<T>::~list()
 {
-	for (node_base* next = end_.next; next != &end_; )
-	{
-		node* del = next->as_node();
-		next = next->next;
-		delete del;
-	}
+    for (node_base* next = end_.next; next != &end_;)
+    {
+        node* del = next->as_node();
+        next      = next->next;
+        delete del;
+    }
 }
 
-template<typename T>
+template <typename T>
 struct list<T>::iterator
 {
-	friend class list;
-	using self_type = iterator;
-	using value_type = T;
-	using reference = T&;
-	using pointer = T*;
-	using iterator_category = std::forward_iterator_tag;
-	using difference_type = std::ptrdiff_t;
-	iterator() = default;
-	iterator(const iterator&) = default;
-	iterator(iterator&&) noexcept = default;
-	iterator(node_base* node) : current_(node) {}
-	~iterator() = default;
-	iterator& operator=(const iterator&) = default;
-	iterator& operator=(iterator&&) noexcept = default;
-	[[nodiscard]] reference operator*() { return current_->as_node()->data; }
-	[[nodiscard]] pointer operator->() { return &current_->as_node()->data; }
-	self_type& operator++() { current_ = current_->next; return *this; }
-	self_type operator++(int) { const self_type ret(current_); current_->next; return ret; }
-	self_type& operator--() { current_ = current_->prev; return *this; }
-	self_type operator--(int) { const self_type ret(current_); current_->prev; return ret; }
-	[[nodiscard]] bool operator==(const self_type& rhs) const { return current_ == rhs.current_; }
-	[[nodiscard]] bool operator!=(const self_type& rhs) const { return current_ != rhs.current_; }
+    friend class list;
+    using self_type               = iterator;
+    using value_type              = T;
+    using reference               = T&;
+    using pointer                 = T*;
+    using iterator_category       = std::forward_iterator_tag;
+    using difference_type         = std::ptrdiff_t;
+    iterator()                    = default;
+    iterator(const iterator&)     = default;
+    iterator(iterator&&) noexcept = default;
+    iterator(node_base* node)
+        : current_(node)
+    {
+    }
+    ~iterator()       = default;
+    iterator& operator=(const iterator&) = default;
+    iterator& operator=(iterator&&) noexcept = default;
+    [[nodiscard]] reference operator*() { return current_->as_node()->data; }
+    [[nodiscard]] pointer operator->() { return &current_->as_node()->data; }
+    self_type& operator++()
+    {
+        current_ = current_->next;
+        return *this;
+    }
+    self_type operator++(int)
+    {
+        const self_type ret(current_);
+        current_->next;
+        return ret;
+    }
+    self_type& operator--()
+    {
+        current_ = current_->prev;
+        return *this;
+    }
+    self_type operator--(int)
+    {
+        const self_type ret(current_);
+        current_->prev;
+        return ret;
+    }
+    [[nodiscard]] bool operator==(const self_type& rhs) const { return current_ == rhs.current_; }
+    [[nodiscard]] bool operator!=(const self_type& rhs) const { return current_ != rhs.current_; }
+
 private:
-	node_base* current_;
+    node_base* current_;
 };
-template<typename T>
+template <typename T>
 struct list<T>::const_iterator
 {
-	friend class list;
-	using self_type = const_iterator;
-	using value_type = T;
-	using reference = const T&;
-	using pointer = const T*;
-	using iterator_category = std::bidirectional_iterator_tag;
-	using difference_type = std::ptrdiff_t;
-	const_iterator() = default;
-	const_iterator(const node_base* node) : current_(node) {}
-	const_iterator(const const_iterator&) = default;
-	const_iterator(const_iterator&&) noexcept = default;
-	~const_iterator() = default;
-	const_iterator& operator=(const const_iterator&) = default;
-	const_iterator& operator=(const_iterator&&) = default;
-	[[nodiscard]] reference operator*() { return current_->as_node()->data; }
-	[[nodiscard]] pointer operator->() { return &current_->as_node()->data; }
-	self_type& operator++() { current_ = current_->next; return *this; }
-	self_type operator++(int) { const self_type ret(current_); current_->next; return ret; }
-	self_type& operator--() { current_ = current_->prev; return *this; }
-	self_type operator--(int) { const self_type ret(current_); current_->prev; return ret; }
-	[[nodiscard]] bool operator==(const self_type& rhs) const { return current_ == rhs.current_; }
-	[[nodiscard]] bool operator!=(const self_type& rhs) const { return current_ != rhs.current_; }
+    friend class list;
+    using self_type         = const_iterator;
+    using value_type        = T;
+    using reference         = const T&;
+    using pointer           = const T*;
+    using iterator_category = std::bidirectional_iterator_tag;
+    using difference_type   = std::ptrdiff_t;
+    const_iterator()        = default;
+    const_iterator(const node_base* node)
+        : current_(node)
+    {
+    }
+    const_iterator(const const_iterator&)     = default;
+    const_iterator(const_iterator&&) noexcept = default;
+    ~const_iterator()                         = default;
+    const_iterator& operator=(const const_iterator&) = default;
+    const_iterator& operator=(const_iterator&&) = default;
+    [[nodiscard]] reference operator*() { return current_->as_node()->data; }
+    [[nodiscard]] pointer operator->() { return &current_->as_node()->data; }
+    self_type& operator++()
+    {
+        current_ = current_->next;
+        return *this;
+    }
+    self_type operator++(int)
+    {
+        const self_type ret(current_);
+        current_->next;
+        return ret;
+    }
+    self_type& operator--()
+    {
+        current_ = current_->prev;
+        return *this;
+    }
+    self_type operator--(int)
+    {
+        const self_type ret(current_);
+        current_->prev;
+        return ret;
+    }
+    [[nodiscard]] bool operator==(const self_type& rhs) const { return current_ == rhs.current_; }
+    [[nodiscard]] bool operator!=(const self_type& rhs) const { return current_ != rhs.current_; }
+
 private:
-	const node_base* current_;
+    const node_base* current_;
 };
 
 template <class T>
 typename list<T>::reference list<T>::front()
 {
-	return end_.next->as_node()->data;
+    return end_.next->as_node()->data;
 }
 
 template <class T>
 typename list<T>::const_reference list<T>::front() const
 {
-	return end_.next->as_node()->data;
+    return end_.next->as_node()->data;
 }
 
 template <class T>
 typename list<T>::reference list<T>::back()
 {
-	return end_.prev->as_node()->data;
+    return end_.prev->as_node()->data;
 }
 
 template <class T>
 typename list<T>::const_reference list<T>::back() const
 {
-	return end_.prev->as_node()->data;
+    return end_.prev->as_node()->data;
 }
 
 template <class T>
 void list<T>::delete_node(node_base* node)
 {
-	node->prev->next = node->next;
-	node->next->prev = node->prev;
-	delete node;
-	--size_;
+    node->prev->next = node->next;
+    node->next->prev = node->prev;
+    delete node;
+    --size_;
 }
 
-template<class T>
+template <class T>
 void list<T>::push_back(const T& val)
 {
-	add_node(end_.prev, &end_, val);
+    add_node(end_.prev, &end_, val);
 }
 
-template<class T>
+template <class T>
 void list<T>::push_back(T&& val)
 {
-	add_node(end_.prev, &end_, std::move(val));
+    add_node(end_.prev, &end_, std::move(val));
 }
 
 template <class T>
 void list<T>::push_front(const T& val)
 {
-	add_node(&end_, end_.next, val);
+    add_node(&end_, end_.next, val);
 }
 
 template <class T>
 void list<T>::push_front(T&& val)
 {
-	add_node(&end_, end_.next, std::move(val));
+    add_node(&end_, end_.next, std::move(val));
 }
 template <class T>
 typename list<T>::iterator list<T>::insert(const_iterator pos, const T& val)
 {
-	return add_node(pos.current_->prev, pos.current_, std::move(val));
+    return add_node(pos.current_->prev, pos.current_, std::move(val));
 }
 
 template <class T>
 typename list<T>::iterator list<T>::insert(const_iterator pos, T&& val)
 {
-	return add_node(pos.current_->prev, pos.current_, std::forward<T>(val));
+    return add_node(pos.current_->prev, pos.current_, std::forward<T>(val));
 }
 
-template<class T>
-template<class ...Args>
-typename list<T>::reference list<T>::emplace_back(Args&& ...args)
+template <class T>
+template <class... Args>
+typename list<T>::reference list<T>::emplace_back(Args&&... args)
 {
-	node* new_node = add_node(end_.prev, &end_, std::forward<Args>(args)...);
-	return new_node->data;
+    node* new_node = add_node(end_.prev, &end_, std::forward<Args>(args)...);
+    return new_node->data;
 }
-template<class T>
-template<class ...Args>
-typename list<T>::reference list<T>::emplace_front(Args&& ...args)
+template <class T>
+template <class... Args>
+typename list<T>::reference list<T>::emplace_front(Args&&... args)
 {
-	node* new_node = add_node(&end_, end_.next, std::forward<Args>(args)...);
-	return new_node->data;
+    node* new_node = add_node(&end_, end_.next, std::forward<Args>(args)...);
+    return new_node->data;
 }
-template<class T>
-template<class ...Args>
-typename list<T>::iterator list<T>::emplace(const_iterator pos, Args&& ...args)
+template <class T>
+template <class... Args>
+typename list<T>::iterator list<T>::emplace(const_iterator pos, Args&&... args)
 {
-	node* new_node = add_node(pos.current_->prev, pos.current_, std::forward<Args>(args)...);
-	return new_node;
+    node* new_node = add_node(pos.current_->prev, pos.current_, std::forward<Args>(args)...);
+    return new_node;
 }
 
-template<class T>
+template <class T>
 void list<T>::reverse() noexcept
 {
-	for (node_base* current = &end_; ;)
-	{
-		node_base* next = current->next;
-		current->next = current->prev;
-		current->prev = next;
-		if (next == &end_)
-			break;
-		current = next;
-	}
+    for (node_base* current = &end_;;)
+    {
+        node_base* next = current->next;
+        current->next   = current->prev;
+        current->prev   = next;
+        if (next == &end_)
+            break;
+        current = next;
+    }
 }
 
 template <class T>
 void list<T>::swap(list& other) noexcept
 {
-	std::swap(size_, other.size_);
-	std::swap(end_, other.end_);
-	if (size_ == 0)
-	{
-		end_.next = end_.prev = end_.as_node();
-	}
-	else
-	{
-		end_.next->prev = end_.prev->next = end_.as_node();
-	}
-	if (other.size_ == 0)
-	{
-		other.end_.next = other.end_.prev = other.end_.as_node();
-	}
-	else
-	{
-		other.end_.next->prev = other.end_.prev->next = other.end_.as_node();
-	}
+    std::swap(size_, other.size_);
+    std::swap(end_, other.end_);
+    if (size_ == 0)
+    {
+        end_.next = end_.prev = end_.as_node();
+    }
+    else
+    {
+        end_.next->prev = end_.prev->next = end_.as_node();
+    }
+    if (other.size_ == 0)
+    {
+        other.end_.next = other.end_.prev = other.end_.as_node();
+    }
+    else
+    {
+        other.end_.next->prev = other.end_.prev->next = other.end_.as_node();
+    }
 }
 
-template<class T>
+template <class T>
 void swap(list<T>& lhs, list<T>& rhs) noexcept(noexcept(lhs.swap(rhs)))
 {
-	lhs.swap(rhs);
+    lhs.swap(rhs);
 }
--- LinkedList/test.cpp
+++ LinkedList/test.cpp
@@ -5,113 +5,137 @@
 
 struct std_s
 {
-	std_s() { count += 1; }
-	std_s(int) { count += 2; }
-	std_s(std_s&&) noexcept { count += 4; }
-	std_s(const std_s&) { count += 8; }
-	std_s& operator=(const std_s&) { count += 16; return *this; }
-	std_s& operator=(std_s&&) noexcept { count += 32; return *this; }
-	~std_s() { count += 64; }
-	static std::size_t count;
+    std_s() { count += 1; }
+    std_s(int) { count += 2; }
+    std_s(std_s&&) noexcept { count += 4; }
+    std_s(const std_s&) { count += 8; }
+    std_s& operator=(const std_s&)
+    {
+        count += 16;
+        return *this;
+    }
+    std_s& operator=(std_s&&) noexcept
+    {
+        count += 32;
+        return *this;
+    }
+    ~std_s() { count += 64; }
+    static std::size_t count;
 };
 
 struct my_s
 {
-	my_s() { count += 1; }
-	my_s(int) { count += 2; }
-	my_s(my_s&&) noexcept { count += 4; }
-	my_s(const my_s&) { count += 8; }
-	my_s& operator=(const my_s&) { count += 16; return *this; }
-	my_s& operator=(my_s&&) noexcept { count += 32; return *this; }
-	~my_s() { count += 64; }
-	static std::size_t count;
+    my_s() { count += 1; }
+    my_s(int) { count += 2; }
+    my_s(my_s&&) noexcept { count += 4; }
+    my_s(const my_s&) { count += 8; }
+    my_s& operator=(const my_s&)
+    {
+        count += 16;
+        return *this;
+    }
+    my_s& operator=(my_s&&) noexcept
+    {
+        count += 32;
+        return *this;
+    }
+    ~my_s() { count += 64; }
+    static std::size_t count;
 };
 
 std::size_t std_s::count = 0;
-std::size_t my_s::count = 0;
+std::size_t my_s::count  = 0;
+
+using Iterator      = list<bool>::iterator;
+using ConstIterator = list<bool>::const_iterator;
+
+static_assert(std::is_copy_constructible_v<ConstIterator>);
+static_assert(std::is_trivially_copy_constructible_v<ConstIterator>);
+
+static_assert(std::is_copy_constructible_v<Iterator>);
+static_assert(std::is_trivially_copy_constructible_v<Iterator>);
 
 void reset_count()
 {
-	std_s::count = 0;
-	my_s::count = 0;
+    std_s::count = 0;
+    my_s::count  = 0;
 }
 
 void test_and_reset()
 {
-	REQUIRE(std_s::count == my_s::count);
-	reset_count();
+    REQUIRE(std_s::count == my_s::count);
+    reset_count();
 }
 
-using my_list = list<my_s>;
+using my_list  = list<my_s>;
 using std_list = std::list<std_s>;
 
 TEST_CASE("Default")
 {
-	my_list a;
-	std_list b;
-	test_and_reset();
+    my_list a;
+    std_list b;
+    test_and_reset();
 }
 TEST_CASE("Emplace")
 {
-	my_list a;
-	std_list b;
-	a.emplace_back(1);
-	a.emplace_front(1);
-	b.emplace_back(1);
-	b.emplace_front(1);
-	test_and_reset();
+    my_list a;
+    std_list b;
+    a.emplace_back(1);
+    a.emplace_front(1);
+    b.emplace_back(1);
+    b.emplace_front(1);
+    test_and_reset();
 }
 TEST_CASE("Push L Value")
 {
-	my_list a;
-	std_list b;
-	const my_s sa;
-	const std_s sb;
-	a.push_back(sa);
-	a.push_front(sa);
-	b.push_back(sb);
-	b.push_front(sb);
-	test_and_reset();
+    my_list a;
+    std_list b;
+    const my_s sa;
+    const std_s sb;
+    a.push_back(sa);
+    a.push_front(sa);
+    b.push_back(sb);
+    b.push_front(sb);
+    test_and_reset();
 }
 TEST_CASE("Push R Value")
 {
-	my_list a;
-	std_list b;
-	a.push_back(my_s());
-	a.push_front(my_s());
-	a.push_back(my_s(1));
-	a.push_front(my_s(1));
-	b.push_back(std_s());
-	b.push_front(std_s());
-	b.push_back(std_s(1));
-	b.push_front(std_s(1));
-	test_and_reset();
+    my_list a;
+    std_list b;
+    a.push_back(my_s());
+    a.push_front(my_s());
+    a.push_back(my_s(1));
+    a.push_front(my_s(1));
+    b.push_back(std_s());
+    b.push_front(std_s());
+    b.push_back(std_s(1));
+    b.push_front(std_s(1));
+    test_and_reset();
 }
 TEST_CASE("Swap")
 {
-	list<int> odd;
-	list<int> even;
-	even.push_back(1);
-	even.push_back(3);
-	odd.push_back(2);
-	odd.push_back(4);
-	swap(odd, even);
-	int one = *odd.begin();
-	int two = *even.begin();
-	int three = *std::next(odd.begin());
-	int four = *std::next(even.begin());
-	REQUIRE(one == 1);
-	REQUIRE(two == 2);
-	REQUIRE(three == 3);
-	REQUIRE(four == 4);
-
+    list<int> odd;
+    list<int> even;
+    even.push_back(1);
+    even.push_back(3);
+    odd.push_back(2);
+    odd.push_back(4);
+    swap(odd, even);
+    int one   = *odd.begin();
+    int two   = *even.begin();
+    int three = *std::next(odd.begin());
+    int four  = *std::next(even.begin());
+    REQUIRE(one == 1);
+    REQUIRE(two == 2);
+    REQUIRE(three == 3);
+    REQUIRE(four == 4);
 }
 TEST_CASE("Swap iterator")
 {
-	list<int> a = { 1,2,3 };
-	list<int> b = { 4,5,6 };
-	auto itera = a.begin();
-	auto iterb = b.begin();
-	std::swap(itera, iterb);
-	REQUIRE(itera == b.begin());
+    list<int> a = {1, 2, 3};
+    list<int> b = {4, 5, 6};
+    auto itera  = a.begin();
+    auto iterb  = b.begin();
+    std::swap(itera, iterb);
+    REQUIRE(itera == b.begin());
 }
